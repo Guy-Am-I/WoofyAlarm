aa9f0f3dbe407b1f31c61b524a3749b16f9284ab
Guy Steinberg <guysteinberg123@gmail.com>
2024-04-20 11:25:14 -0700

    Added main screen tab view with background colors and tab icons

---
+++ TimeOfDayIcon.swift
@@ -0,0 +1,18 @@
+//
+//  TimeOfDayIcon.swift
+//  WoofyAlarm
+//
+//  Created by Guy Steinberg on 2024-04-20.
+//
+
+import SwiftUI
+
+struct TimeOfDayIcon: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TimeOfDayIcon()
+}
--- WoofyAlarm/Views/MainViews/MainAlarmView.swift
+++ WoofyAlarm/Views/MainViews/MainAlarmView.swift
@@ -2,15 +2,31 @@
 import SwiftUI
 
 struct MainAlarmView: View {
-    let textLocalTest: LocalizedStringKey = "Hello World"
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text(textLocalTest)
+        TabView {
+            Group {
+                Text("Placeholder Sleep View")
+                    .tabItem {
+                        Label("Alarms", systemImage: "moon")
+                    }
+                Text("Placeholder Sounds View")
+                    .tabItem {
+                        Label("Sounds", systemImage: "music.quarternote.3")
+                    }
+                Text("Placeholder Settings View")
+                    .tabItem {
+                        Label("Settings", systemImage: "gearshape.fill")
+                    }
+                Text("Placeholder About View")
+                    .tabItem {
+                        Label("About", systemImage: "info.circle")
+                    }
+            }
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+            .background(Asset.backgroundPrimary.color)
+            .toolbarBackground(.visible, for: .tabBar)
+            .toolbarBackground(Asset.backgroundTabs.color, for: .tabBar)
         }
-        .padding()
     }
 }
 
