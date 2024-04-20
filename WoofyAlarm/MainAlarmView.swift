89f14af8915be76e57f03ee24fdfd4d38e9377a9
Guy Steinberg <guysteinberg123@gmail.com>
2024-04-20 07:41:23 -0700

    fix folder heirachy and added app icon

---
+++ WoofyAlarm/MainAlarmView.swift
@@ -0,0 +1,18 @@
+
+import SwiftUI
+
+struct MainAlarmView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hdsello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    MainAlarmView()
+}
---
+++ WoofyAlarm/WoofyAlarmApp.swift
@@ -0,0 +1,17 @@
+//
+//  WoofyAlarmApp.swift
+//  WoofyAlarm
+//
+//  Created by Guy Steinberg on 2024-04-19.
+//
+
+import SwiftUI
+
+@main
+struct WoofyAlarmApp: App {
+    var body: some Scene {
+        WindowGroup {
+            MainAlarmView()
+        }
+    }
+}
