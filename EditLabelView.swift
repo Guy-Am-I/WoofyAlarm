095b262c6ca2a22431e4e2ac3004cfb01552f3de
Guy Steinberg <guysteinberg123@gmail.com>
2024-04-20 14:30:36 -0700

    Added edit label view for editing sleep schedule

---
+++ EditLabelView.swift
@@ -0,0 +1,27 @@
+//
+//  EditLabelView.swift
+//  WoofyAlarm
+//
+//  Created by Guy Steinberg on 2024-04-20.
+//
+
+import SwiftUI
+
+struct EditLabelView: View {
+    var body: some View {
+        RoundedRectangle(cornerRadius: 20)
+            .fill(.clear)
+            .stroke(Asset.buttonPrimary.color, lineWidth: 3)
+            .overlay {
+                Image(systemName: "square.and.pencil")
+                    .font(.system(size: 30))
+                    .offset(y: -4)
+                    
+            }
+            .frame(width: 80, height: 60)
+    }
+}
+
+#Preview {
+    EditLabelView()
+}
--- WoofyAlarm/Views/MainViews/BedtimeView.swift
+++ WoofyAlarm/Views/MainViews/BedtimeView.swift
@@ -8,18 +8,43 @@
 import SwiftUI
 
 struct BedtimeView: View {
+    
     var body: some View {
         VStack(alignment: .leading) {
             Text("Sleep Time")
+                .font(.headline)
             HStack {
-                Text("08:30")
-                    .font(.system(size: 50))
-                    .fontWeight(.bold)
+                BedtimeDurationView(hour: 8, minute: 30)
                 Spacer()
-                Image(systemName: "square.and.pencil.circle")
+                EditLabelView()
             }
+            .padding(.trailing)
         }
         .padding(.vertical)
+    }
+}
+
+struct BedtimeDurationView: View {
+    let hour: Int
+    let minute: Int
+    
+    var body: some View {
+        HStack(alignment: .bottom) {
+            Text(hour < 10 ? "0\(hour)" : "\(hour)")
+                .font(.system(size: 60))
+                .fontWeight(.bold)
+            Text("hr")
+                .font(.title)
+                .fontWeight(.thin)
+                .padding(.bottom, 10)
+            Text("\(minute)")
+                .font(.system(size: 60))
+                .fontWeight(.bold)
+            Text("min")
+                .font(.title)
+                .fontWeight(.thin)
+                .padding(.bottom, 10)
+        }
     }
 }
 
